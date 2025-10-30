cc3a21428f394a910aca9e68d4c43a613bd083a7
agnestshibangu <agnes.tshibangugrillon@gmail.com>
2025-10-30 14:27:42 +0100

    vault script wip

--- vault/vault-start.sh
+++ vault/vault-start.sh
@@ -7,15 +7,6 @@ VAULT=${VAULT:-vault}
 vault server -config=/vault/config/config.hcl &  # lance Vault
 VAULT_PID=$!
 
-# echo "⏳ Attente que Vault soit accessible..."
-# while true; do
-#   STATUS_JSON=$($VAULT status -format=json 2>/dev/null || true)
-#   echo "$STATUS_JSON" | grep -q '"sealed"' && break
-#   echo "Vault pas encore prêt..."
-#   sleep 2
-# done
-# echo "✅ Vault répond à l’API HTTP."
-
 sh /vault/vault-init.sh
 
 wait $VAULT_PID  # garder Vault comme PID1
